8a1fc0dbcca8f13a14c6a0a6cf64cbbf8b7f5c02
Youstina Ibrahim <youstinaaibrahim@gmail.com>
2020-08-10 11:53:48 -0400

    YoustinaReactPortfolio

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import Resume from './Components/Resume';
 import Contact from './Components/Contact';
 import Testimonials from './Components/Testimonials';
 import Portfolio from './Components/Portfolio';
+import resumeData from './resumeData.json';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      foo: 'bar',
-      resumeData: {}
+      resumeData
     };
 
     ReactGA.initialize('UA-110570651-1');
@@ -24,24 +24,24 @@ class App extends Component {
 
   }
 
-  getResumeData() {
-    $.ajax({
-      url: 'resumeData.json',
-      dataType: 'json',
-      cache: false,
-      success: function (data) {
-        this.setState({ resumeData: data});
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
+  // getResumeData() {
+  //   $.ajax({
+  //     url: 'resumeData.json',
+  //     dataType: 'json',
+  //     cache: false,
+  //     success: function (data) {
+  //       this.setState({ resumeData: data});
+  //     }.bind(this),
+  //     error: function (xhr, status, err) {
+  //       console.log(err);
+  //       alert(err);
+  //     }
+  //   });
+  // }
 
-  componentDidMount() {
-    this.getResumeData();
-  }
+  // componentDidMount() {
+  //   this.getResumeData();
+  // }
 
   render() {
     return ( 
